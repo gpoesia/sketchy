42981085beb7cf888c42e57e44c1b9543947bfad
scottviteri <scottviteri@gmail.com>
2019-12-15 18:12:06 -0700

    Separated full test case

--- test_constraints.py
+++ test_constraints.py
@@ -7,16 +7,12 @@ import subprocess
 
 EXAMPLE = IF_TRUE
 
-linear_combo_ssa_node = ssa(EXAMPLE)
+ssa_node = ssa(EXAMPLE)
 
 # print_visitor_2 = ASTPrinter()
-# walk(linear_combo_ssa_node, print_visitor_2)
+# walk(ssa_node, print_visitor_2)
 
 constraint_visitor = ConstraintVisitor()
-walk(linear_combo_ssa_node, constraint_visitor)
-constraints = constraint_visitor.outputConstraints(linear_combo_ssa_node)
+walk(ssa_node, constraint_visitor)
+constraints = constraint_visitor.outputConstraints(ssa_node)
 print(constraints)
-
-#with open('out.smt','w') as f:
-#    f.write(constraints)
-#print(subprocess.check_output(['z3','-smt2','out.smt']))
---
+++ test_full.py
@@ -0,0 +1,18 @@
+from translate import *
+from printer import *
+from ssa import *
+from constraints import *
+from examples import *
+import subprocess
+
+for EXAMPLE in [LINEAR_COMBINATION, IF_TRUE, IF_FALSE, IF_TRUE_ELSE_NOTHING]:
+    ssa_node = ssa(EXAMPLE)
+    # print_visitor_2 = ASTPrinter()
+    # walk(ssa_node, print_visitor_2)
+    constraint_visitor = ConstraintVisitor()
+    walk(ssa_node, constraint_visitor)
+    constraints = constraint_visitor.outputConstraints(ssa_node)
+    #print(constraints)
+    with open('out.smt','w') as f:
+        f.write(constraints)
+    print(subprocess.check_output(['z3','-smt2','out.smt']))
